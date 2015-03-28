79cd15afadf30f5e0d96d5ef2c531cacd2c27166
Dražen Lučanin <kermit666@gmail.com>
2015-03-28 16:13:29 +0100

    mv public public-old; mv admin public; mv bower_components

--- public/js/main.js
+++ public/js/main.js
@@ -1,57 +1,73 @@
-/**
- * Now let's start our AngularJS app...
- * which uses RequireJS to load  packages and code
- *
- *
- * Created by emileharmel on 16/07/2014.
- *
- */
-(function (define)
-{
-	"use strict";
+'use strict';
 
-	define([
-		'domReady',
-		'utils/logger/ExternalLogger',
-		'utils/logger/LogDecorator',
-		'auth/AuthenticateModule',
-		'dashboard/DashboardModule',
-		'app/RouteManager'
-	], function (domReady, $log, LogDecorator, AuthenticateModule, DashboardModule, RouteManager)
-	{
+/* Controllers */
 
+app
+  .controller('AppCtrl', ['$scope', '$translate', '$localStorage', '$window',
+    function(              $scope,   $translate,   $localStorage,   $window ) {
+      // add 'ie' classes to html
+      var isIE = !!navigator.userAgent.match(/MSIE/i);
+      isIE && angular.element($window.document.body).addClass('ie');
+      isSmartDevice( $window ) && angular.element($window.document.body).addClass('smart');
 
+      // config
+      $scope.app = {
+        name: 'cloud fleet',
+        version: '1.0.1',
+        // for chart colors
+        color: {
+          primary: '#e26826',
+          info:    '#088076',
+          success: '#27c24c',
+          warning: '#fad733',
+          danger:  '#f05050',
+          light:   '#e8eff0',
+          dark:    '#3a3f51',
+          black:   '#1c2b36'
+        },
+        settings: {
+          themeID: 1,
+          navbarHeaderColor: 'bg-cf-orange',
+          asideColor: 'bg-black',
+          headerFixed: true,
+          asideFixed: true,
+          asideFolded: true
+        }
+      };
 
+      // save settings to local storage
+      if ( angular.isDefined($localStorage.settings) ) {
+        $scope.app.settings = $localStorage.settings;
+      } else {
+        $localStorage.settings = $scope.app.settings;
+      }
+      $scope.$watch('app.settings', function(){
+        if( $scope.app.settings.asideDock  &&  $scope.app.settings.asideFixed ){
+          // aside dock and fixed must set the header fixed.
+          $scope.app.settings.headerFixed = true;
+        }
+        // save to local storage
+        $localStorage.settings = $scope.app.settings;
+      }, true);
 
-		require(['domReady!'], function (document) {
+      // angular translate
+      $scope.lang = { isopen: false };
+      $scope.langs = {en:'English', de_DE:'German'};
+      $scope.selectLang = $scope.langs[$translate.proposedLanguage()] || "English";
+      $scope.setLang = function(langKey, $event) {
+        // set the current lang
+        $scope.selectLang = $scope.langs[langKey];
+        // You can change the language during runtime
+        $translate.use(langKey);
+        $scope.lang.isopen = !$scope.lang.isopen;
+      };
 
-			var app, appName = 'cloudfleet.cockpit';
+      function isSmartDevice( $window )
+      {
+          // Adapted from http://www.detectmobilebrowsers.com
+          var ua = $window['navigator']['userAgent'] || $window['navigator']['vendor'] || $window['opera'];
+          // Checks for iOs, Android, Blackberry, Opera Mini, and Windows mobile devices
+          return (/iPhone|iPod|iPad|Silk|Android|BlackBerry|Opera Mini|IEMobile/).test(ua);
+      }
 
-			$log = $log.getInstance("BOOTSTRAP");
-			$log.debug("Initializing {0}", [ appName ]);
-
-		/**
-		 * Start the main application
-		 *
-		 * We manually start this bootstrap process; since ng:app is gone
-		 * ( necessary to allow Loader splash pre-AngularJS activity to finish properly )
-		 */
-
-
-			app = angular.module(appName, ["ui.router", "ngSanitize", "ngResource", "ngCookies", AuthenticateModule, DashboardModule ])
-				.config(LogDecorator)
-				.config(RouteManager);
-
-			try {
-				angular.bootstrap(document.getElementsByTagName("html")[0], [ appName ]);
-			} catch (e) {
-				$log.debug(e.stack || e.message || e);
-			}
-
-			return app;
-
-		});
-
-	});
-
-}(define));
+  }]);
