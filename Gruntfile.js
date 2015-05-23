bd0db8447966d7e444c490d5e77088d6f8a6d86e
Christoph Witzany <christoph@web.crofting.com>
2015-05-23 10:05:21 +0200

    Do not cdnify libs

--- Gruntfile.js
+++ Gruntfile.js
@@ -358,12 +358,6 @@ module.exports = function (grunt) {
       }
     },
 
-    // Replace Google CDN references
-    cdnify: {
-      dist: {
-        html: ['<%= yeoman.dist %>/*.html']
-      }
-    },
 
     // Copies remaining files to places other tasks can use
     copy: {
@@ -514,7 +508,6 @@ module.exports = function (grunt) {
     'concat',
     'ngAnnotate',
     'copy:dist',
-    'cdnify',
     'cssmin',
     'uglify',
     'filerev',
