7be25af5e9447fb0fa9c6edea50dab4ac1c2483a
Christoph Witzany <christoph@web.crofting.com>
2015-09-10 12:23:33 +0200

    start working on profile page

---
+++ app/scripts/controllers/profile.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name blimpCockpitApp.controller:ProfileController
+ * @description
+ * # AboutCtrl
+ * Controller of the SigninFormController
+ */
+angular.module('blimpCockpitApp')
+  .controller('ProfileController',
+  ['$scope', '$http', '$state', 'cockpitApi',
+    function($scope, $http, $state, cockpitApi) {
+	$scope.changePassword = function() {
+	};
+	$scope.updateUser = function() {
+	};
+  }]);
